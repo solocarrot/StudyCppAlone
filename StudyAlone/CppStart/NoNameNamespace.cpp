a07ab98007aa79c5b92ea5610801cc950282cb6f
solocarrot <rlawhddbs3339@gmail.com>
2026-08-07 10:58:20 +0900

    "예제1번 따라해봄"

---
+++ StudyAlone/CppStart/NoNameNamespace.cpp
@@ -0,0 +1,14 @@
+//#include <iostream>
+//
+//namespace {
+//	//이함수는 이 파일안에서만	사용할수있음
+//	int OnlyThisFile() {
+//	}
+//
+//	int only_this_file = 0;
+//}
+//
+//int main() {
+//	OnlyThisFile();
+//	only_this_file = 3;
+//}
--- StudyAlone/CppStart/hello.cpp
+++ StudyAlone/CppStart/hello.cpp
@@ -1,7 +1,7 @@
-#include <iostream>
-
-int main() {
-	std::cout << "Hello, World1!!!" << std::endl;
-	//std는 iostream에 있는 std라는 이름의 공간을 가져온것이다. cout는 자바의 sout랑 비슷하고 endl은 함수이다. 
-	return 0;
-}
+//#include <iostream>
+//
+//int main() {
+//	std::cout << "Hello, World1!!!" << std::endl;
+//	//std는 iostream에 있는 std라는 이름의 공간을 가져온것이다. cout는 자바의 sout랑 비슷하고 endl은 함수이다. 
+//	return 0;
+//}
