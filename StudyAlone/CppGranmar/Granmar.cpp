366c7cb019852b22d22533310bc0e94f4fcc3c21
solocarrot <rlawhddbs3339@gmail.com>
2026-08-07 23:41:25 +0900

    "switch문 만들어봄"

--- StudyAlone/CppGranmar/Granmar.cpp
+++ StudyAlone/CppGranmar/Granmar.cpp
@@ -21,6 +21,8 @@
 //	return 0;
 //}
 
+//while문
+
 //#include <iostream>
 //
 //int main() {
@@ -34,24 +36,58 @@
 //	std::cout << "합은" << sum << std::endl;
 //	return 0;
 //}
+//
+// 입력받기
+// 
+//#include <iostream>
+//
+//int main() {
+//	int luckyNumber = 3;
+//	std::cout << "내 비밀번호를 맞춰보세요" << std::endl;
+//
+//	int user_input;
+//
+//	while (1) {
+//		std::cout << "입력 : ";
+//		std::cin >> user_input;
+//		if (luckyNumber == user_input) {
+//			std::cout << "맞추셨습니다" << std::endl;
+//			break;
+//		} else {
+//			std::cout << "틀렸습니다" << std::endl;
+//		}
+//	}
+//	return 0;
+//}
 
-#include <iostream>
-
-int main() {
-	int luckyNumber = 3;
-	std::cout << "내 비밀번호를 맞춰보세요" << std::endl;
-
-	int user_input;
-
-	while (1) {
-		std::cout << "입력 : ";
-		std::cin >> user_input;
-		if (luckyNumber == user_input) {
-			std::cout << "맞추셨습니다" << std::endl;
-			break;
-		} else {
-			std::cout << "틀렸습니다" << std::endl;
-		}
-	}
-	return 0;
-}
+// switch문 이용하기
+//#include <iostream>
+//
+//using std::cout;
+//using std::endl;
+//using std::cin;
+//
+//int main() {
+//	int user_input;
+//	cout << "저의 정보를 표시해줍시다" << endl;
+//	cout << "1. 이름" << endl;
+//	cout << "2. 나이 "  << endl;
+//	cout << "3. 성별" << endl;
+//	cin >> user_input;
+//
+//	switch (user_input) {
+//		case 1:
+//			cout << "종윤" << endl;
+//			break;
+//		case 2:
+//			cout << "24살" << endl;
+//			break;
+//		case 3:
+//			cout << "남자" << endl;
+//			break;
+//		default:
+//			cout << "궁금한게없군요" << endl;
+//			break;
+//
+//	}
+//}
