549717834ca939125ac53275f4b16cd7c3a7eabc
solocarrot <rlawhddbs3339@gmail.com>
2026-08-07 23:17:47 +0900

    "while문 만들어봄"

--- StudyAlone/CppGranmar/Granmar.cpp
+++ StudyAlone/CppGranmar/Granmar.cpp
@@ -1,10 +1,36 @@
+//#include <iostream>
+//
+//int main() {
+//	int i;
+//
+//	for (i = 0; i < 10; i++) {
+//		std::cout << i << std::endl;
+//	}
+//	return 0;
+//}
+
+//#include <iostream>
+//
+//int main() {
+//	int i, sum = 0;
+//
+//	for (i = 1; i < 10; i++) {
+//		sum += i;
+//	}
+//	std::cout << sum << std::endl;
+//	return 0;
+//}
+
 #include <iostream>
 
 int main() {
-	int i;
+	int i = 1, sum = 0;
 
-	for (i = 0; i < 10; i++) {
-		std::cout << i << std::endl;
+	while (i <= 10) {
+		sum += i;
+		i++;
 	}
+
+	std::cout << "합은" << sum << std::endl;
 	return 0;
 }
