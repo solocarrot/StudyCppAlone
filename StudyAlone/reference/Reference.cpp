11b78804f5c09c90ce4be5aa061595173e8825a6
solocarrot <rlawhddbs3339@gmail.com>
2026-08-10 03:05:53 +0900

    "함수의 인자로 참조자를 가지면 메인함수에서 넣는 참조자 = 매개변수 가되서 아주아름다워진다"

--- StudyAlone/reference/Reference.cpp
+++ StudyAlone/reference/Reference.cpp
@@ -27,9 +27,26 @@
 //	std::cout << "another_a = " << another_a << std::endl;
 //}
 
-//래퍼런스이해+
-int a = 10;
-int &another_a = a;
+////래퍼런스이해+
+//int a = 10;
+//int &another_a = a;
+//
+//int b = 3;
+//another_a = b;
 
-int b = 3;
-another_a = b;
+//인자가 된 참조자
+#include <iostream>
+
+int change_val(int& p) {
+	p = 3;
+
+	return 0;
+}
+
+int main() {
+	int number = 5;
+
+	std::cout << number << std::endl;
+	change_val(number);
+	std::cout << number << std::endl;
+}
