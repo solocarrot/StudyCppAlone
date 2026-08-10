8b0b9d979ce72a9b8d753606f991c4066b6d27a5
solocarrot <rlawhddbs3339@gmail.com>
2026-08-10 17:16:44 +0900

    "int b = fuction(); 에서 무슨일이 일어났냐면 fuction에서정의된 값이 복사가 되었따. 복사가중요함. fuction이 종료되면 a는 메모리에서 사라짐."

--- StudyAlone/reference/Reference.cpp
+++ StudyAlone/reference/Reference.cpp
@@ -59,18 +59,29 @@
 //
 //	std::cout << ref << std::endl;
 //}
+//
+////배열래퍼런스
+//#include <iostream>
+//
+//int main() {
+//	int arr[] = { 1,2,3 };
+//	int(&ref)[3] = arr;
+//
+//	ref[0] = 2;
+//	ref[1] = 3;
+//	ref[2] = 1;
+//
+//	std::cout << arr[0] << arr[1] << arr[2] << std::endl;
+//	return 0;
+//}
 
-//배열래퍼런스
-#include <iostream>
+//래퍼런스를 리턴하는 함수
+int fuction() {
+	int a = 2;
+	return a;
+}
 
 int main() {
-	int arr[] = { 1,2,3 };
-	int(&ref)[3] = arr;
-
-	ref[0] = 2;
-	ref[1] = 3;
-	ref[2] = 1;
-
-	std::cout << arr[0] << arr[1] << arr[2] << std::endl;
+	int b = fuction();
 	return 0;
 }
