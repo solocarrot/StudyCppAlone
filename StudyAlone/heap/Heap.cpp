dc6959cfd995f04298355d2ba6a4e174705eaa34
solocarrot <rlawhddbs3339@gmail.com>
2026-08-11 20:14:18 +0900

    "int 배열만들고 값넣고 출력하고

--- StudyAlone/heap/Heap.cpp
+++ StudyAlone/heap/Heap.cpp
@@ -1,12 +1,31 @@
-//new와 delete의 사용
+////new와 delete의 사용
+//#include <iostream>
+//
+//int main() {
+//	int* p = new int;
+//	*p = 10;
+//
+//	std::cout << *p << std::endl;
+//
+//	delete p;
+//	return 0;
+//}
+
 #include <iostream>
 
 int main() {
-	int* p = new int;
-	*p = 10;
+	int arr_size;
+	std::cout << "arr size = ";
+	std::cin >> arr_size;
+	int *list = new int[arr_size];
 
-	std::cout << *p << std::endl;
+	for (int i = 0; i < arr_size; i++) {
+		std::cin >> list[i];
+	}
 
-	delete p;
+	for (int i = 0; i < arr_size; i++) {
+		std::cout << i << "th element of list = " << list[i] << std::endl;
+	}
+	delete list;
 	return 0;
 }
