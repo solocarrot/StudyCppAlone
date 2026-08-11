c00ce7a8029a7cf3d47ffcbfe1ed6098bc587fc1
solocarrot <rlawhddbs3339@gmail.com>
2026-08-11 20:18:29 +0900

    "변수의 생존범위를 확인했음 내부에서 a = 3;으로 설정해도 중괄호가 끝나는 시점에서 이 정보는 삭제됨"

--- StudyAlone/heap/Heap.cpp
+++ StudyAlone/heap/Heap.cpp
@@ -1,4 +1,4 @@
-////new와 delete의 사용
+﻿////new와 delete의 사용
 //#include <iostream>
 //
 //int main() {
@@ -11,21 +11,37 @@
 //	return 0;
 //}
 
+//#include <iostream>
+//
+//int main() {
+//	int arr_size;
+//	std::cout << "arr size = ";
+//	std::cin >> arr_size;
+//	int *list = new int[arr_size];
+//
+//	for (int i = 0; i < arr_size; i++) {
+//		std::cin >> list[i];
+//	}
+//
+//	for (int i = 0; i < arr_size; i++) {
+//		std::cout << i << "th element of list = " << list[i] << std::endl;
+//	}
+//	delete list;
+//	return 0;
+//}
+
+//변수의 존재범위
+
 #include <iostream>
 
 int main() {
-	int arr_size;
-	std::cout << "arr size = ";
-	std::cin >> arr_size;
-	int *list = new int[arr_size];
-
-	for (int i = 0; i < arr_size; i++) {
-		std::cin >> list[i];
+	int a = 4;
+	{
+	std::cout << "외부의 변수 1 :" << a << std::endl;
+	int a = 3;
+	std::cout << "내부의 변수:" << a << std::endl;
 	}
 
-	for (int i = 0; i < arr_size; i++) {
-		std::cout << i << "th element of list = " << list[i] << std::endl;
-	}
-	delete list;
+	std::cout << "외부의 변수 2:" << a << std::endl;
 	return 0;
 }
