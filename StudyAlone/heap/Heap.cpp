51f2591a915607a628e0bc56d163f6ebc20a0d7d
solocarrot <rlawhddbs3339@gmail.com>
2026-08-12 17:27:05 +0900

    "동물추가해보고있는데 구조체랑 포인터라는 걸 처음봤음"

--- StudyAlone/heap/Heap.cpp
+++ StudyAlone/heap/Heap.cpp
@@ -29,19 +29,68 @@
 //	delete list;
 //	return 0;
 //}
+//
+////변수의 존재범위
+//
+//#include <iostream>
+//
+//int main() {
+//	int a = 4;
+//	{
+//	std::cout << "외부의 변수 1 :" << a << std::endl;
+//	int a = 3;
+//	std::cout << "내부의 변수:" << a << std::endl;
+//	}
+//
+//	std::cout << "외부의 변수 2:" << a << std::endl;
+//	return 0;
+//}
 
-//변수의 존재범위
+//마이펫만들기
 
 #include <iostream>
 
-int main() {
-	int a = 4;
-	{
-	std::cout << "외부의 변수 1 :" << a << std::endl;
-	int a = 3;
-	std::cout << "내부의 변수:" << a << std::endl;
-	}
+typedef struct Animal {
+	char name[30];
+	int age;
 
-	std::cout << "외부의 변수 2:" << a << std::endl;
-	return 0;
+	int health;
+	int food;
+	int clean;
+} Animal;
+
+void create_animal(Animal* animal) {
+	std::cout << "동물의 이름 = ";
+	std::cin >> animal->name;
+
+	std::cout << "동물의 나이 = ";
+	std::cin >> animal->age;
+
+	animal->health = 100;
+	animal->food = 100;
+	animal->clean = 100;
+}
+
+void play(Animal* animal) {
+	animal-> health += 10;
+	animal-> food -= 20;
+	animal-> clean -= 30;
+}
+
+void one_day_pass(Animal* animal) {
+	// 하루가지나면
+	animal->health -= 10;
+	animal->food -= 30;
+	animal->clean -= 20;
+}
+
+void show_stat(Animal* animal) {
+	std::cout << "동물의 상태" << std::endl;
+	std::cout << "체력: " << animal->health << std::endl;
+	std::cout << "청결: " << animal->clean << std::endl;
+	std::cout << "배부름: " << animal->food << std::endl;
+}
+int main() {
+	Animal* list[10];
+	int anmial_num = 0;
 }
