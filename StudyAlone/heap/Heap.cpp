3e0c29c74f74785a99e9f27a9e76f4a83775a023
solocarrot <rlawhddbs3339@gmail.com>
2026-08-13 21:43:59 +0900

    "새를 만들면 새에 heigt라는 변수떄문에 모든 코드를 바꿔야되는 문제가발생한다. 그래서 객체지향프로그램이중요하다."

--- StudyAlone/heap/Heap.cpp
+++ StudyAlone/heap/Heap.cpp
@@ -46,51 +46,51 @@
 //	return 0;
 //}
 
-//마이펫만들기
-
-#include <iostream>
-
-typedef struct Animal {
-	char name[30];
-	int age;
-
-	int health;
-	int food;
-	int clean;
-} Animal;
-
-void create_animal(Animal* animal) {
-	std::cout << "동물의 이름 = ";
-	std::cin >> animal->name;
-
-	std::cout << "동물의 나이 = ";
-	std::cin >> animal->age;
-
-	animal->health = 100;
-	animal->food = 100;
-	animal->clean = 100;
-}
-
-void play(Animal* animal) {
-	animal-> health += 10;
-	animal-> food -= 20;
-	animal-> clean -= 30;
-}
-
-void one_day_pass(Animal* animal) {
-	// 하루가지나면
-	animal->health -= 10;
-	animal->food -= 30;
-	animal->clean -= 20;
-}
-
-void show_stat(Animal* animal) {
-	std::cout << "동물의 상태" << std::endl;
-	std::cout << "체력: " << animal->health << std::endl;
-	std::cout << "청결: " << animal->clean << std::endl;
-	std::cout << "배부름: " << animal->food << std::endl;
-}
-int main() {
-	Animal* list[10];
-	int anmial_num = 0;
-}
+////마이펫만들기
+//
+//#include <iostream>
+//
+//typedef struct Animal {
+//	char name[30];
+//	int age;
+//
+//	int health;
+//	int food;
+//	int clean;
+//} Animal;
+//
+//void create_animal(Animal* animal) {
+//	std::cout << "동물의 이름 = ";
+//	std::cin >> animal->name;
+//
+//	std::cout << "동물의 나이 = ";
+//	std::cin >> animal->age;
+//
+//	animal->health = 100;
+//	animal->food = 100;
+//	animal->clean = 100;
+//}
+//
+//void play(Animal* animal) {
+//	animal-> health += 10;
+//	animal-> food -= 20;
+//	animal-> clean -= 30;
+//}
+//
+//void one_day_pass(Animal* animal) {
+//	// 하루가지나면
+//	animal->health -= 10;
+//	animal->food -= 30;
+//	animal->clean -= 20;
+//}
+//
+//void show_stat(Animal* animal) {
+//	std::cout << "동물의 상태" << std::endl;
+//	std::cout << "체력: " << animal->health << std::endl;
+//	std::cout << "청결: " << animal->clean << std::endl;
+//	std::cout << "배부름: " << animal->food << std::endl;
+//}
+//int main() {
+//	Animal* list[10];
+//	int anmial_num = 0;
+//}
