419ff63c67e4a36f868b69126e0bce21423e0532
solocarrot <rlawhddbs3339@gmail.com>
2026-08-16 01:25:51 +0900

    "생성자에 대해서 알아보았다 자바와 비슷한 부분이 많은듯하다"

---
+++ StudyAlone/constructerAndOverloading/Constructer.cpp
@@ -0,0 +1,23 @@
+//생성자
+
+#include <iostream>
+
+
+class Date {
+	int year_;
+	int month_;
+	int day_;
+
+public:
+	void SetDate(int year, int month, int day);
+	void AddDay(int inc);
+	void AddMonth(int inc);
+	void AddYear(int inc);
+	void ShowData();
+
+	Date(int year, int month, int day) {
+		year_ = year;
+		month_ = month;
+		day_ = day;
+	}
+};
